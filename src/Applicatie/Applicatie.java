315417388f496784c5164e329b2770e674f1d001
Groothedde, Jeroen <Jeroen.Groothedde@kadaster.nl>
2018-06-08 14:38:50 +0200

    Ik denk dat bij jou commit niet alles mee gegaan naar github. Je wil alles automatisch laten tracken. Anders upload je maar een gedeelte. Er was geen Beurt() klasse. Even een nieuw voorbeeld met wat tips.
    - Let goed op de scope van de snorretjes {}. Zorg er voor dat er niet te veel zijn en dat alles mooi sluit.
    - intellij heeft rechts onderin GIT functionaliteit. kun je switchen tussen Master en Branches
    - Rechtsbovenin heb je een knopje met een blauw pijltje waar (update project) op staat, hier kun je mee check of er een nieuwe versie op GIT staat
    - CTRL + K = commit
    - CTRAL + SHIFT + K = push naar GIT

--- src/Applicatie/Applicatie.java
+++ src/Applicatie/Applicatie.java
@@ -2,13 +2,14 @@ package Applicatie;
 
 import java.util.Random;
 import java.util.Scanner;
-//OOP == Object Oriented Programming
-//Dit is een vrij lege opzet voor mastermind in OOP stijl
 
 public class Applicatie {
 	static Bord bord =  new Bord();
 
 	public static void main(String[] args) {
-		//Q: lege main, maar toch wordt er iets geprint: hoe kan dit? ;-)
+		Beurt.genereerAntwoord();
+		while (Beurt.beurt < 12) {
+			Beurt.speelBeurt();
+		}
 	}
 }
